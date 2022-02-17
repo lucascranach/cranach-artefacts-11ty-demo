f306cf785d3a8469f09a08bfa09fa15eb74e98fb
cnoss <christian.noss@th-koeln.de>
2022-02-17 15:28:14 +0100

    created EN Version for graphics

---
+++ src/graphics-virtual-objects.en.11ty.js
@@ -0,0 +1,18 @@
+exports.data = {
+  layout: 'graphic-virtual-object.11ty.js',
+  lang: 'en',
+  collectionID: 'graphicsVirtualObjectsEN',
+  entityType: 'graphicsVirualObject',
+  pagination: {
+    data: 'collections.graphicsVirtualObjectsEN',
+    size: 1,
+    alias: 'graphicsMasterData',
+    currentCollection: 'collections.graphicsVirtualObjectsEN',
+  },
+  permalink: function (data) {
+    const item = data.pagination.items[0];
+    return `/${data.lang}/graphics/${item.metadata.id}/`;
+  },
+};
+
+exports.render = (data) => data.pagination.items[0];
