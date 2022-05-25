fa5c186618bf8ec87ac00cae8dbe0e765cec0bb3
cnoss <christian.noss@th-koeln.de>
2022-05-25 22:59:26 +0200

    cleaned up files

--- .eleventy.js
+++ .eleventy.js
@@ -187,7 +187,7 @@ const appendToFile = (path, str) => {
 
 const getPaintingsCollection = (lang) => {
   const paintingsForLang = paintingsData[lang];
-  const devObjects = ["DE_HMR_KN1992-8", "RO_MNB_217", "DE_KsDW_I-51"];
+  const devObjects = ["DE_SKD_GG1918","DE_BStGS_WAF166","CH_SORW_1925-1b", "DE_HMR_KN1992-8", "RO_MNB_217", "DE_KsDW_I-51"];
 
   const paintings = process.env.ELEVENTY_ENV === 'production'
     ? paintingsForLang.items
