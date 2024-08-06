cf3eef88b947cb580174abdbab666a3995a37b43
Yannic Brügger <yannic.bruegger@gmail.com>
2024-08-06 14:39:03 +0200

    fix faulty existence checks

--- src/_layouts/components/overall-overview.11ty.js
+++ src/_layouts/components/overall-overview.11ty.js
@@ -1,15 +1,8 @@
 exports.getOverallOverview = (eleventy, { content }, langCode) => {
   const imageStack = content.images;
-
-  if(!imageStack
-    || !imageStack['overall']
-    || !imageStack['overall']['infos']
-    || !imageStack['overall']['infos']['hasOverallOverview']) {
-    return '';
-  }
-
-  const overallOverview = imageStack['overall'].images.filter(image => image.id.match(/Overall_Overview/));
-  const imageData = overallOverview[0].sizes['small'];
+  const overallOverview = imageStack?.overall?.images?.filter((image) => image.id.match(/Overall_Overview/));
+  if (overallOverview === undefined || overallOverview?.length === 0) return '';
+  const imageData = overallOverview[0].sizes.small;
 
   return `
     <figure class="overall-overview">
--- src/_layouts/components/representant-image.11ty.js
+++ src/_layouts/components/representant-image.11ty.js
@@ -3,14 +3,14 @@ exports.getRepresentant = (eleventy, { content }) => {
   const alt = content.metadata.title;
 
   const getImageDimensions = () => {
-    if (!content.images) return '';
-    if (!content.images.overall) return '';
-    const { width, height } = content.images.overall.images[0].sizes.small.dimensions;
+    const dimensions = content?.images?.overall?.images[0]?.sizes?.small?.dimensions;
+    if (dimensions === undefined) return '';
+    const { width, height } = dimensions;
     return `width="${width}" height="${height}"`;
   };
 
   const imageDimensions = getImageDimensions();
-  
+
   return `
     <figure class="leporello-recog__image">
       <img loading="lazy" src="${src}" alt="${eleventy.altText(alt)}"${imageDimensions}>
